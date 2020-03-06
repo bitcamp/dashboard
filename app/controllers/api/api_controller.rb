cc58f9f23fb70f92a9f3a87ebfcc088a0c2c9209
Evan McIntire <mcintire.evan@gmail.com>
2020-03-06 17:18:24 -0500

    person lookup

--- app/controllers/api/api_controller.rb
+++ app/controllers/api/api_controller.rb
@@ -2,8 +2,12 @@ module Api
   class Api::ApiController < ApplicationController
   include DeviseTokenAuth::Concerns::SetUserByToken
 
-  skip_before_action :auth_user
+  protect_from_forgery with: :null_session, if: -> { request.format.json? }
   before_action :configure_permitted_parameters, if: :devise_controller?
+  before_action :auth_user
+  before_action :set_raven_context
+  after_action  :set_access_control_headers
+  after_action  :set_extra_headers
 
   def configure_permitted_parameters
     devise_parameter_sanitizer.permit(:sign_in, keys: [:email, :password])
@@ -11,6 +15,37 @@ module Api
 
   def index
     render json: {}
+  end
+
+  def lookup_user
+    return head :unauthorized if @current_user.nil?
+    return head :bad_request if request.body.nil?
+
+    r = JSON.parse request.body.read
+
+    return head :bad_request if r["email"].nil?
+
+    user = User.where(:email => r["email"]).first
+
+    return head :not_found if user.nil?
+
+    j = user.to_json
+
+    z = {}
+
+
+    if user.has_applied?
+      app = EventApplication.where(:user_id => user.id).first
+      z["food_restrictions"] = app["food_restrictions"]
+      z["food_restrictions_info"] = app["food_restrictions_info"]
+      z["t_shirt_size"] = app["t_shirt_size"]
+    end
+
+    x = {
+      "user": user,
+    }.merge(z)
+
+    render json: x
   end
 end
 end
